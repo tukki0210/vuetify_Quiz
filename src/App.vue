f62e131f2de54fdff3bd3a86f113fb346fa57ac3
tukki0210 <tukki0210@gmail.com>
2023-12-14 14:22:11 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-main>
+      <Quiz quiz_text="quiz_test1" />
+      <Quiz quiz_text="quiz_test2" />
+      <Quiz quiz_text="quiz_test3" />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Quiz from './components/Quiz.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Quiz
+  }
+}
+</script>
---
+++ src/components/Quiz.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-card class="mx-auto my-8" max-width="344" elevation="16">
+        <v-card-item>
+            <v-card-title>
+                Card title
+            </v-card-title>
+            <v-card-subtitle>
+                Card subtitle secondary text
+            </v-card-subtitle>
+        </v-card-item>
+
+        <v-card-text>
+           {{ quiz_text }}
+        </v-card-text>
+    </v-card>
+</template>
+<script>
+export default {
+    name: 'Quiz',
+    props: {
+        quiz_text: String
+    }
+}
+</script>
